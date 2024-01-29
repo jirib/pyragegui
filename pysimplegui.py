d63c8e9155b1847b1a8e6d46a7d2cddb4248ffe8
Jiří Bělka <jirib79@gmail.com>
2024-01-29 13:40:44 +0100

    add a way to load key.py/encrypt.py/decrypt.py (windows)

--- pysimplegui.py
+++ pysimplegui.py
@@ -1,4 +1,8 @@
 import PySimpleGUI as sg
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
 from key import *
 from encrypt import *
 from decrypt import *
